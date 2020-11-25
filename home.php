a49bd9813c61f83d78ed9393df7eb188155df079
Tu-Luc Duong <1835233@edu.vaniercollege.qc.ca>
2020-11-25 13:11:19 -0500

    parallax kinda added

--- home.php
+++ home.php
@@ -1,8 +1,17 @@
-<h2>Knucklehead & Chucklehead Locker</h2>
-<h2>Sports equipment</h2>
-<h2>Hockey</h2>
-<h2>Volleyball</h2>
-<h2>Basketball</h2>
-<h2>Baseball</h2>
-<h2>Badminton</h2>
-<h2>Tennis</h2>
+<div id="homepage">
+    <div class="homeTitles"><h2>Knucklehead & Chucklehead Locker</h2></div>
+    <div class="homeTitles"><h2>Footwear</h2></div>
+    <div class="homeImage" id="homeImage1"></div>
+    <div class="homeTitles"><h2>Hockey</h2></div>
+    <div class="homeImage" id="homeImage2"></div>
+    <div class="homeTitles"><h2>Volleyball</h2></div>
+    <div class="homeImage" id="homeImage3"></div>
+    <div class="homeTitles"><h2>Basketball</h2></div>
+    <div class="homeImage" id="homeImage4"></div>
+    <div class="homeTitles"><h2>Baseball</h2></div>
+    <div class="homeImage" id="homeImage5"></div>
+    <div class="homeTitles"><h2>Badminton</h2></div>
+    <div class="homeImage" id="homeImage6"></div>
+    <div class="homeTitles"><h2>Tennis</h2></div>
+    <div class="homeImage" id="homeImage7"></div>
+</div>
