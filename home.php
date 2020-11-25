7181d6ae9c3a1d21e0fd52e419270527ca78da0c
Tu-Luc Duong <1835233@edu.vaniercollege.qc.ca>
2020-11-25 13:14:45 -0500

    parallax done

--- home.php
+++ home.php
@@ -1,6 +1,8 @@
 <div id="homepage">
-    <div class="homeTitles"><h2>Knucklehead & Chucklehead Locker</h2></div>
-    <div class="homeTitles"><h2>Footwear</h2></div>
+    <div class="homeTitles">
+        <h2>Knucklehead & Chucklehead Locker</h2>
+        <h2>Footwear</h2>
+    </div>    
     <div class="homeImage" id="homeImage1"></div>
     <div class="homeTitles"><h2>Hockey</h2></div>
     <div class="homeImage" id="homeImage2"></div>
