afdb8def873a7437ec47c0bd9704162f9d3a80ac
James-Gabriel Cortez-Gregoire <1836197@edu.vaniercollege.qc.ca>
2020-11-27 22:53:07 -0500

    logout button appears when user signs in, with cart icon and account tab (dashboard)

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="KCLocker.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>K&C Locker</title>
@@ -34,6 +35,9 @@
                 break;
                 case 'createAccount':
                     include_once('createAccount.php');
+                break;
+                case 'dashboard':
+                    include_once('dashboard.php');
                 break;
                 default:
                     break;
