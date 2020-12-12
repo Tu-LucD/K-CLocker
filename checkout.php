4ea7d5b6a8ac059160a073ba0c5c8dc5bfa87eea
Tu-Luc Duong <1835233@edu.vaniercollege.qc.ca>
2020-12-12 16:05:30 -0500

    cart and checkout are done

--- checkout.php
+++ checkout.php
@@ -23,18 +23,165 @@
                 //Add item price to order subtotal
                 $subtotal += $lineTotal;
             }
+
+            $tps = round(($subtotal * 0.05), 2);
+            $tvq = round(($subtotal * 0.09975), 2)
         ?>
     </table>
     <h3 id="checkoutSubtotal">Subtotal: <?php echo "$$subtotal" ?></h3>
 </div>
 <div id="checkoutRightDiv">
-    <h3>Receipt</h3>
-    <table id="transactionInfo">
-        <tr>
-            <td>Subtotal:</td>
-            <td><?php echo "$$subtotal";?></td>
-        </tr>
-    </table>
-    <p id="orderTotal">Total:</p>
-    <a href="index.php?page=dashboard"><button id="payButton" class="btn btn-info">Pay</button></a>
+    <form action="" method="post">
+        <h3>Receipt</h3>
+        <table id="transactionInfo">
+            <tr>
+                <td>Subtotal:</td>
+                <td><?php echo "$$subtotal";?></td>
+            </tr>
+            <tr>
+                <td>TPS:</td>
+                <td><?php echo "$$tps"?></td>
+            </tr>
+            <tr>
+                <td>TVQ:</td>
+                <td><?php echo "$$tvq"?></td>
+            </tr>
+            <tr>
+                <td>Shipping Fee & Location:</td>
+                <td>
+                    <select name="location">
+                        <option value="5.00">Montreal ($5.00)</option>
+                        <option value="6.00">Laval ($6.00)</option>
+                        <option value="10.00">Quebec City ($10.00)</option>
+                        <option value="15.00">Granby ($15.00)</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>----------</td>
+            </tr>
+            <tr>
+                <td>Promotions</td>
+            </tr>
+            <?php
+                $promo1 = 0;
+                $promo2 = 0;
+                $promo3 = 0;
+                $promo4 = 0;
+
+                //Checks eligibility for Big Spender's Paradise promotion
+                if($subtotal >= 600)
+                {
+                    $promoRes = mysqli_query($link, "SELECT * FROM promotions where id = 1");
+                    $promoName = mysqli_fetch_array($promoRes)['title'];
+                    $promo1 = round(($subtotal * 0.1),2);
+                    echo "<tr>";
+                    echo "<td>"; echo $promoName; echo "</td>";
+                    echo "<td>"; echo "-$$promo1"; echo "</td>";
+                    echo "</tr>";
+                }
+                
+                //Checks eligibility for Beginner's Luck promotion
+                $userId = $_SESSION['id'];
+                $previousOrders = mysqli_query($link, "SELECT * FROM orders WHERE account_id = $userId");
+                if(!mysqli_fetch_array($previousOrders))
+                {
+                    $promoRes = mysqli_query($link, "SELECT * FROM promotions where id = 2");
+                    $promoName = mysqli_fetch_array($promoRes)['title'];
+                    $promo2 = round(($subtotal * 0.05),2);
+                    echo "<tr>";
+                    echo "<td>"; echo $promoName; echo "</td>";
+                    echo "<td>"; echo "-$$promo2"; echo "</td>";
+                    echo "</tr>";
+                }
+
+                //Checks eligibility for Team Spirit promotion
+                $res = mysqli_query($link, "select * FROM cart");
+                $elligible = false;
+                while($row = mysqli_fetch_array($res))
+                {
+                    if($row['quantity'] >= 10)
+                    {
+                        $elligible = true;
+                    }
+                }
+                if($elligible)
+                {
+                    $promoRes = mysqli_query($link, "SELECT * FROM promotions where id = 3");
+                    $promoName = mysqli_fetch_array($promoRes)['title'];
+                    
+                    $quantity = 0;
+                    $res = mysqli_query($link, "SELECT * FROM cart where quantity >= 10");
+                    while($row = mysqli_fetch_array($res))
+                    {                        
+                        $unitPrice = $row['unityPrice'];
+                    }
+                    $promo3 = round(($unitPrice * 2),2);
+                    echo "<tr>";
+                    echo "<td>"; echo $promoName; echo "</td>";
+                    echo "<td>"; echo "-$$promo3"; echo "</td>";
+                    echo "</tr>";
+                }
+
+                //Checks eligibility for Small Errand promotion
+                if($subtotal <= 20)
+                {
+                    $promoRes = mysqli_query($link, "SELECT * FROM promotions where id = 4");
+                    $promoName = mysqli_fetch_array($promoRes)['title'];
+                    $promo4 = round(($subtotal * 0.05),2);
+                    echo "<tr>";
+                    echo "<td>"; echo $promoName; echo "</td>";
+                    echo "<td>"; echo "-$$promo4"; echo "</td>";
+                    echo "</tr>";
+                }   
+            ?>
+        </table>
+
+        
+        <?php
+        // Calculates final total of the order
+                $orderTotal = $subtotal + $tps + $tvq - $promo1 - $promo2 - $promo3 - $promo4;          
+                // $today = date("Y-m-d");
+                $today = strval(date('Y-m-d'));
+                echo $today;      
+        ?>
+        <p id="orderTotal">Total: <?php echo $orderTotal;?> + Shipping</p>    
+            <input type="submit" id="payButton" name="payButton" class="btn btn-success" value="Pay">
+    </form>    
 </div>
+
+<?php
+    if(isset($_POST['payButton']))
+    {                
+        //Inserting the order in orders table
+        $shipping = $_POST['location'];        
+        mysqli_query($link, "INSERT INTO orders VALUES(NULL, $userId, CURDATE(), $orderTotal, $shipping)");
+
+        //Inserting the items in order_items table
+        $res = mysqli_query($link,"SELECT * FROM orders where account_id = $userId order by id");
+        $orderId = 0;
+        //Get Id of inserted order
+        while($row = mysqli_fetch_array($res))
+        {
+            $orderId = $row['id'];
+        }
+
+        $res = mysqli_query($link, "SELECT * FROM cart");
+        $productId = 0;
+        $quantity = 0;
+        $price = 0;
+        while($row = mysqli_fetch_array($res))
+        {
+            $productId = $row['productId'];
+            $quantity = $row['quantity'];
+            $price = $row['unityPrice'];
+            mysqli_query($link,"INSERT INTO order_items VALUES(NULL, $orderId, $productId, $quantity, $price)");
+        }        
+        mysqli_query($link,"DELETE FROM cart where productId is not null");  
+        ?>
+        <script type=text/javascript>
+            window.location="index.php?page=dashboard";
+        </script> 
+        <?php
+    }
+?>
